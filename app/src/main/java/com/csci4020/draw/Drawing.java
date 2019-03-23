fbb4b416164e1aa09292a3bc863aa84c0468900b
Hannie Kim <hanniek00@gmail.com>
2019-03-23 12:04:38 -0500

    Create Class that will contain the drawing features

---
+++ app/src/main/java/com/csci4020/draw/Drawing.java
@@ -0,0 +1,22 @@
+package com.csci4020.draw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Drawing extends View {
+
+    public Drawing(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+
+    public Drawing(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+
+    }
+    public Drawing(Context context) {
+        super(context);
+    }
+}
+
