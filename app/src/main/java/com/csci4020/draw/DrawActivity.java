4226dd9756382149cb3dc96aac8fb3aca071399c
Hannie Kim <hanniek00@gmail.com>
2019-03-24 18:40:35 -0500

    Set ids for all ui elements

--- app/src/main/java/com/csci4020/draw/DrawActivity.java
+++ app/src/main/java/com/csci4020/draw/DrawActivity.java
@@ -1,6 +1,17 @@
 package com.csci4020.draw;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 public class DrawActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_draw);
+
+
+    }
 }
---
+++ app/src/main/java/com/csci4020/draw/Drawing.java
@@ -0,0 +1,22 @@
+package com.csci4020.draw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+public class Drawing {
+
+//    public Drawing(Context context) {
+//        super(context);
+//        setup();
+//    }
+//
+//    public Drawing(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//        setup();
+//    }
+//
+//    public Drawing(Context context, AttributeSet attrs, int defStyleAttr) {
+//        super(context, attrs, defStyleAttr);
+//        setup();
+//    }
+}
