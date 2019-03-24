5a21c7545e5617c35d3d2096ecf9c2006facdeec
Hannie Kim <hanniek00@gmail.com>
2019-03-24 18:46:29 -0500

    Add click listeners to buttons

--- app/src/main/java/com/csci4020/draw/DrawActivity.java
+++ app/src/main/java/com/csci4020/draw/DrawActivity.java
@@ -3,6 +3,7 @@ package com.csci4020.draw;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 
 public class DrawActivity extends Activity {
 
@@ -13,5 +14,45 @@ public class DrawActivity extends Activity {
         setContentView(R.layout.activity_draw);
 
 
+    }
+
+    private void setUpButtonClickListeners() {
+
+        findViewById(R.id.open_file_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.save_file_Button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.send_message_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        findViewById(R.id.undo_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        
+
+        findViewById(R.id.new_page_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/csci4020/draw/Drawing.java
+++ app/src/main/java/com/csci4020/draw/Drawing.java
@@ -2,21 +2,22 @@ package com.csci4020.draw;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 
-public class Drawing {
+public class Drawing extends View {
 
-//    public Drawing(Context context) {
-//        super(context);
+    public Drawing(Context context) {
+        super(context);
 //        setup();
-//    }
-//
-//    public Drawing(Context context, AttributeSet attrs) {
-//        super(context, attrs);
+    }
+
+    public Drawing(Context context, AttributeSet attrs) {
+        super(context, attrs);
 //        setup();
-//    }
-//
-//    public Drawing(Context context, AttributeSet attrs, int defStyleAttr) {
-//        super(context, attrs, defStyleAttr);
+    }
+
+    public Drawing(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
 //        setup();
-//    }
+    }
 }
