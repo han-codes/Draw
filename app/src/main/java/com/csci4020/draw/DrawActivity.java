65e205cd5085a2327d2edb07a186ed9694e67ca2
Hannie Kim <hanniek00@gmail.com>
2019-03-24 20:55:19 -0500

    initial setup for onCheckedChanged to determine which feature is tapped

--- app/src/main/java/com/csci4020/draw/DrawActivity.java
+++ app/src/main/java/com/csci4020/draw/DrawActivity.java
@@ -1,6 +1,7 @@
 package com.csci4020.draw;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -58,13 +59,46 @@ public class DrawActivity extends Activity implements RadioGroup.OnCheckedChange
             }
         });
 
+
         findViewById(R.id.color_picker_button).setOnClickListener(new View.OnClickListener() {
 
         });
+
+        findViewById(R.id.new_page_button).setOnClickListener(new View.OnClickListener() {
+
+        });
+
+        // set listeners to Radio Group
+        RadioGroup radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(this);
+
+        findViewById(R.id.radioButton_sticker).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        findViewById(R.id.radioButton_frame).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
+    /**
+     * Determine which radioButton feature is checked by the user
+     */
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
 
+//        switch checkedId {
+//            case R.id.radioButton_brush:
+//            case R.id.radioButton_line:
+//            case R.id.radioButton_square:
+//            case R.id.radioButton_sticker:
+//            case R.id.radioButton_frame:
+//        }
     }
 }
