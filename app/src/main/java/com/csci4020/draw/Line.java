c010d649ab8982cfc5e226b4eb8bfaea6a187450
Brian Lake <bnlake@gmail.com>
2019-03-26 21:48:47 -0500

    Started Line class implementing Shape

---
+++ app/src/main/java/com/csci4020/draw/Line.java
@@ -0,0 +1,32 @@
+package com.csci4020.draw;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+public class Line implements Shape
+{
+	private int startX;
+	private int startY;
+	private int endX;
+	private int endY;
+
+	public Line(int startX, int startY, int endX, int endY)
+	{
+		this.startX = startX;
+		this.startY = startY;
+		this.endX = endX;
+		this.endY = endY;
+	}
+
+	@Override
+	public void draw(Canvas canvas, Paint paint)
+	{
+		canvas.drawLine(startX, startY, endX, endY, paint);
+	}
+
+	@Override
+	public int getColor()
+	{
+		return 0;
+	}
+}
