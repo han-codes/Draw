91f2391020d2063759a5fa776d8134c10e44a4fb
Brian Lake <bnlake@gmail.com>
2019-03-26 21:24:10 -0500

    Started class for paint area

---
+++ app/src/main/java/com/csci4020/draw/PaintArea.java
@@ -0,0 +1,23 @@
+package com.csci4020.draw;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class PaintArea extends View
+{
+	public PaintArea(Context context)
+	{
+		super(context);
+	}
+
+	public PaintArea(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+	}
+
+	public PaintArea(Context context, AttributeSet attrs, int defStyleAttr)
+	{
+		super(context, attrs, defStyleAttr);
+	}
+}
